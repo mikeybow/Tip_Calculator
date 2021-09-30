5f9e8c5b3c83a55ab959f8f08b6904a918b6cad8
mikeybow <micbowman25@gmail.com>
2021-09-30 14:02:03 -0500

    editing

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ tip_as_percent = tip / 100
 total_tip_amount = bill * tip_as_percent
 total_bill = bill + total_tip_amount
 bill_per_person = total_bill / num_people
-final_amount = round(bill_per_person, 2)
+final_amount = "{:.2f}".format(bill_per_person)
 
 print(f"Each person should pay: ${final_amount} dollars")
